d95f3461a11726aa7ca5e89c19bb03f6094b1a76
xunruan <xunruan@icloud.com>
2022-08-17 22:46:57 -0400

    register views to send and grab form to html

--- users/views.py
+++ users/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from .forms import CustomUserCreationForm
 
 
 # Create your views here.
 def dashboard(request):
     return render(request, "users/dashboard.html")
+
+
+def register(request):
+    if request.method == 'GET':
+        # pass the user creation form to html to collect user data
+        return render(request, "users/register.html", {"form": CustomUserCreationForm})
+    elif request.method == 'POST':
+        # get the collect that we get from the form
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse('dashboard'))
